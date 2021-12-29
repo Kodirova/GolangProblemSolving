2c012696a331bdec57137b83156b2c2215bc8e30
Kodirova <kodirovairoda7@gmail.com>
2021-12-29 20:41:18 +0500

    package installed, connection to database

--- Structs/models.go
+++ Structs/models.go
@@ -1,6 +1,12 @@
-package structs
+package main
 
-import "time"
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/lib/pq"
+)
 
 type Contact struct {
 	ID        int
@@ -19,4 +25,29 @@ type Task struct {
 	CreatedAt time.Time
 	CreatedBy time.Time
 	DueDate   time.Time
+}
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "iroda1808"
+	dbname   = "test"
+)
+
+func main() {
+	//connection to database
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected")
 }
