221891089a474f544426716161c831baef7abeff
Kodirova <kodirovairoda7@gmail.com>
2022-01-06 15:27:59 +0500

    feat: get, getAll, delete functions added

--- CRUD/structs.go
+++ CRUD/structs.go
@@ -35,17 +35,33 @@ func createContact(c Contact) {
 
 }
 
+func getAll() {
+	for key, value := range ContactList {
+		fmt.Println("Key:", key, "Value:", value)
+	}
+}
+
+func get(n int) {
+	fmt.Println("we are here")
+	value, exists := ContactList[n]
+	fmt.Printf("key exists in map: %t, value: %v \n", exists, value)
+}
+
 func main() {
 	ContactList = make(map[int]*Contact)
+	var firstname, lastname, phone, email, postion string
 menu:
 	var choice int
 	fmt.Println("MENU")
+	fmt.Println("1.Create Contact")
+	fmt.Println("2.Find Contact")
+	fmt.Println("3. List of Contacts")
+	fmt.Println("4. Delete Contact")
 	fmt.Println("Choose")
 	fmt.Scanf("%d", &choice)
 
 	switch choice {
 	case 1:
-		var firstname, lastname, phone, email, postion string
 		fmt.Println("enter first name")
 		fmt.Scanf("%s", &firstname)
 		fmt.Println("enter last name")
@@ -69,5 +85,22 @@ menu:
 		fmt.Println("contact declared", contact)
 		createContact(contact)
 		goto menu
+	case 2:
+		var search_id int
+		fmt.Println("enter your id:")
+		fmt.Scanf("%d", &search_id)
+		get(search_id)
+		goto menu
+	case 3:
+		getAll()
+		goto menu
+	case 4:
+		var delete_id int
+		fmt.Println("enter your id:")
+		fmt.Scanf("%d", &delete_id)
+		delete(ContactList, delete_id)
+		fmt.Println("Succesfully deleted")
+		goto menu
 	}
+
 }
